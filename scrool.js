021d6f6c5ba2b4b3dda49f1244c6838a66359c2e
Chertopologov <Dillerfeed99@yandex.ru>
2025-01-15 04:19:52 +0300

    main

--- scrool.js
+++ scrool.js
@@ -18,3 +18,14 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   });
 });
+
+
+function triggerDownload(fileName) {
+    var element = document.createElement('a');
+    element.setAttribute('href', fileName);
+    element.setAttribute('download', fileName);
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+  }
