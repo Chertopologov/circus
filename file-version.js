cb6c7f4551f33b8c64073fed604428e9263704c7
Chertopologov <Dillerfeed99@yandex.ru>
2025-01-19 02:50:37 +0300

    download button

--- file-version.js
+++ file-version.js
@@ -1,15 +1,6 @@
-function getVersionFromFile(fileName) {
-    const regex = /V(\d+\.\d+\.\d+)/;
-    const match = fileName.match(regex);
-    return match ? match[1] : 'Версия не найдена';
+function setFileName(fileName) {
+    document.getElementById('file-version').textContent = fileName;
 }
 
-function updateFileVersion() {
-    const downloadButton = document.querySelector('.button');
-    const fileName = downloadButton.getAttribute('onclick').match(/'([^']+)'/)[1]; // Извлекаем имя файла из onclick
-    const version = getVersionFromFile(fileName);
-    const versionElement = document.getElementById('file-version');
-    versionElement.textContent = ` ${fileName} - Версия: ${version}`;
-}
-
-document.addEventListener('DOMContentLoaded', updateFileVersion);
+// Пример использования:
+setFileName("Circus V8.exe");
