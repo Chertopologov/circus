12c1fe0e579947d1a4d50c88144c4dacbcdab58b
Chertopologov <dillerfeed99@yandex.ru>
2025-01-28 04:04:05 +0300

    circus 2.0

--- script.js
+++ script.js
@@ -3,12 +3,51 @@ function setFileName(fileName) {
     document.getElementById('file-version').textContent = fileName;
 }
 setFileName("Circus V8.exe");
+// SLiDER
+const slider = document.querySelector('.slider');
+const slides = document.querySelectorAll('.slide');
+const dots = document.querySelectorAll('.dot');
+const prevButton = document.querySelector('.slider-prev');
+const nextButton = document.querySelector('.slider-next');
 
+let currentSlide = 0;
 
-document.addEventListener('mousemove', e => {
-	w = window.innerWidth;
-	h = window.innerHeight;
-	x = Math.round(e.pageX / w * 100);
-	y = Math.round(e.pageY / h * 100);
-	document.body.style.background = `radial-gradient(at ${x}% ${y}%, #6b5ef7, #f75e5e)`;
+function showSlide(index) {
+    if(index < 0 || index >= slides.length){
+        return;
+    }
+    slider.style.transform = `translateX(-${index * 600}px)`;
+    dots.forEach(dot => dot.classList.remove('active'));
+    dots[index].classList.add('active');
+    currentSlide = index;
+}
+
+dots.forEach((dot, index) => {
+    dot.addEventListener('click', () => {
+        showSlide(index);
+    });
 });
+
+prevButton.addEventListener('click', () => {
+  showSlide(currentSlide - 1);
+});
+
+nextButton.addEventListener('click', () => {
+  showSlide(currentSlide + 1);
+});
+// DOWNLOAD
+const downloadButton = document.getElementById('button');
+
+downloadButton.addEventListener('click', function() {
+    const filename = this.getAttribute('data-filename');
+    triggerDownload(filename);
+});
+
+function triggerDownload(filename) {
+    const link = document.createElement('a');
+    link.href = filename;
+    link.download = filename;
+     document.body.appendChild(link);
+    link.click();
+   document.body.removeChild(link);
+}
